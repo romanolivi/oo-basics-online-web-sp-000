660b8f1f71e726a7e065ff8018f243c182a1bd63
Romano Livi <romano_livi@outlook.com>
2020-12-16 20:20:45 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,10 +1,11 @@
 class Shoe 
   
-  attr_accessor :color, :size, :material, :condition
-  attr_reader :brand 
+  attr_accessor :color, :size, :material
+  attr_reader :brand, :condition
   
-  def initialize(brand)
+  def initialize(brand, condition)
     @brand = brand
+    @condition = condition
   end
   
   def cobble 

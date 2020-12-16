33bf4c3280a0d7ca722a58f5008961284f775114
Romano Livi <romano_livi@outlook.com>
2020-12-16 20:15:24 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -4,7 +4,7 @@ class Shoe
   
   def initialize(brand)
     @brand = brand
-  endhttps://learn.co/tracks/full-stack-web-development-v8/module-6-object-oriented-ruby/section-5-object-lifecycle/oo-basics#
+  end
   
   
   
